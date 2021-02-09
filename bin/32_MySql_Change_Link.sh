2389ffd872ea32585c8bc41520ad3a9893971984
Leonardo Gentile <leonard.gentile@gmail.com>
2021-02-09 20:20:27 +0100

    Remove old stuff

--- bin/32_MySql_Change_Link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo rm /usr/local/mysql
-sudo ln -s /usr/local/mysql-5.1.53-osx10.6-x86/ /usr/local/mysql
-
--- bin/64_MySql_Change_Link.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo rm /usr/local/mysql
-sudo ln -s /usr/local/mysql-5.5.11-osx10.6-x86_64/ /usr/local/mysql
